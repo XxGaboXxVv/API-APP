87d65713aaf4bc6fe8e876e61febf1c1c42e63ec
XxGaboXxVv <128988968+XxGaboXxVv@users.noreply.github.com>
2024-08-27 18:17:36 -0600

    Update index2.js

--- index2.js
+++ index2.js
@@ -14,11 +14,11 @@ const app = express();
 app.use(bp.json());
 
 const mysqlPool = mysql.createPool({
-     host:'srv1059.hstgr.io',
-    user:'u729991132_root',
-    password:'Dragonb@ll2',
-    database:'u729991132_railway',
-    port:3306,
+     host:'junction.proxy.rlwy.net',
+    user:'root',
+    password:'KIrqtUpHbSvuQinLjxJIVrlxFZCqqxnl',
+    database:'railway',
+    port:17751,
      multipleStatements: true
 
 });
