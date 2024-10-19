3fad34cd286e286c13d1e08c5fb2c548a246d3c7
XxGaboXxVv <128988968+XxGaboXxVv@users.noreply.github.com>
2024-10-19 01:33:35 -0600

    Update index2.js

--- index2.js
+++ index2.js
@@ -14,14 +14,12 @@ const app = express();
 app.use(bp.json());
 
 const mysqlPool = mysql.createPool({
-  host:'82.197.82.66',
-  user:'u995289331_root',
-  password:'CodeM@sters123',
-  database:'u995289331_railway',
-  port:3306,
-  waitForPools: true,
-  PoolLimit: 0, // Ajusta según el rendimiento y necesidades
-  queueLimit: 0
+    host:'82.197.82.66',
+    user:'u995289331_root',
+    password:'CodeM@sters123',
+    database:'u995289331_railway',
+    port:3306,
+    multipleStatements: true
 });
 
 // SERVIDOR DE CORREO 
